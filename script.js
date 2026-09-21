2745a2fec95037053ab5c7057b469c8a73b0e511
Gonzabernal03 <127991610+Gonzabernal03@users.noreply.github.com>
2026-09-21 14:30:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -466,7 +466,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // ==========================================
 
 // Reemplazá este enlace entre comillas por la URL que copiaste en el Paso 3
-const API_URL_SHEETS = "https://script.google.com/macros/s/AKfycbx03zzROcMPlnQGCbsA7xhu3XNZxcu03_FQ1ca4ghaqXbgP_pIEWM8Ny4ngbBRwrBnMsQ/exec";
+const API_URL_SHEETS = "https://script.google.com/macros/s/AKfycbxNoG7paVD1rvBcznQdBZEq3A3VmN0YQMvxQDxxXzVH6UdJpDvhI-wrSL-ARCKL3fAMmQ/exec";
 
 // Función para enviar registro de cliente a Google Sheets
 function enviarClienteAGoogle(nombre, apellido, email, telefono) {
