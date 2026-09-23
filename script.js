1beab48dcce5aedd5ebeaf67d55b23de63cadcef
Gonzabernal03 <127991610+Gonzabernal03@users.noreply.github.com>
2026-09-23 11:52:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ==================== */
 
 // CONFIG
-const PRODUCTS_API_URL = 'https://script.google.com/macros/s/AKfycbxGceDLg_UOUy4GABtYaN_x46M8TS-9-0K17oQC4SgripnirT5KLFfOiq2IiFh-1KpN/exec'; // Cambiar por tu API de Google Apps Script
+const PRODUCTS_API_URL = 'https://script.google.com/macros/s/AKfycbxHuBRGbOM4zs9wH5n-7OLNUprUzw0Pvvscj0U_1dC-GiTZ6UdVsbXgZz4Np0ZNq6rF/exec'; // Cambiar por tu API de Google Apps Script
 const SHEET_API_BASE = PRODUCTS_API_URL;
 const WHATSAPP_NUMBER = '5492325590916';
 
