d2aa7705a6910526b6e1a5feaae653f2b3f99046
Gonzabernal03 <127991610+Gonzabernal03@users.noreply.github.com>
2026-09-24 11:09:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -407,7 +407,7 @@ updateCartCount();
 const heroBg = document.querySelector('.hero-bg');
 
 const heroImages = [
-  'https://i.postimg.cc/Ghc9hYDP/Whats-App-Image-2026-09-15-at-19-40-52.jpg',
+  'https://i.postimg.cc/6QqmqFpG/of.jpg',
   'https://i.postimg.cc/yx76x4kp/Whats-App-Image-2026-09-14-at-18-11-46.jpg',
   'https://i.postimg.cc/2yTkBcWd/Whats-App-Image-2026-09-14-at-18-07-24.jpg',
 ];
